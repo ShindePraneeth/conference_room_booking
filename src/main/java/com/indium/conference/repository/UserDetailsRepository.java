f279a90f18e16b0ef42592cb8d83700266391213
Lakshmipriyaindium <126647108+Lakshmipriyaindium@users.noreply.github.com>
2024-12-21 20:17:02 +0530

    Booking and Audit service

--- src/main/java/com/indium/conference/repository/UserDetailsRepository.java
+++ src/main/java/com/indium/conference/repository/UserDetailsRepository.java
@@ -1,13 +1,20 @@
 package com.indium.conference.repository;
 
+import com.indium.conference.entity.AuditLog;
 import com.indium.conference.entity.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserDetailsRepository extends JpaRepository<UserDetails, String> {
     @Query("SELECT u FROM UserDetails u WHERE u.emailId = :emailId")
     UserDetails findByEmailId(@Param("emailId") String emailId);
+
+
+
 }
